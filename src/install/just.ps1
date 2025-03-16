09e9ca2822ef0e2c0c10f657b71b0f4db037663c
Macklan Weinstein <macklan.weinstein@gmail.com>
2025-03-16 16:50:24 -0700

    Simplify CI workflow if statements

---
+++ src/install/just.ps1
@@ -0,0 +1,16 @@
+<#
+.SYNOPSIS
+    Install Just for Windows systems.
+#>
+
+# If unable to execute due to policy rules, run
+# Set-ExecutionPolicy RemoteSigned -Scope CurrentUser.
+
+# Exit immediately if a PowerShell cmdlet encounters an error.
+$ErrorActionPreference = 'Stop'
+# Disable progress bar for PowerShell cmdlets.
+$ProgressPreference = 'SilentlyContinue'
+# Exit immediately when an native executable encounters an error.
+$PSNativeCommandUseErrorActionPreference = $True
+
+choco install --yes just
