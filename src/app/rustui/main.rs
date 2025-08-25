3b1cf6029584602cf4ba29099aa68be17326c62e
Macklan Weinstein <macklan.weinstein@gmail.com>
2025-08-24 23:58:09 -0700

    Fix Rustui webview for Linux

--- src/app/rustui/main.rs
+++ src/app/rustui/main.rs
@@ -5,6 +5,8 @@
 //! eyre = "^0.6.0"
 //! tao = "^0.34.2"
 //! wry = "^0.53.0"
+//! [target.'cfg(target_os = "linux")'.dependencies]
+//! gtk = "^0.18.2"
 //! ```
 
 use clap::{Arg, ArgAction, Command};
@@ -15,14 +17,37 @@ use tao::{
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     platform::unix::WindowExtUnix,
-    window::WindowBuilder,
+    window::{Window, WindowBuilder},
 };
-use wry::{WebViewBuilder, WebViewBuilderExtUnix};
+use wry::{WebView, WebViewBuilder, WebViewBuilderExtUnix};
+
+fn build_webview(window: &Window, debug: bool) -> eyre::Result<WebView> {
+    let html = load_html()?;
+    let builder = WebViewBuilder::new().with_devtools(debug).with_html(&html);
+
+    #[cfg(not(target_os = "linux"))]
+    let webview = builder.build(&window)?;
+    #[cfg(target_os = "linux")]
+    let webview = {
+        use gtk::prelude::WidgetExt;
+        let vbox = window
+            .default_vbox()
+            .ok_or_eyre("Unable to get program window.")?;
+        vbox.set_hexpand(true);
+        vbox.set_vexpand(true);
+        vbox.show();
+        builder.build_gtk(&vbox.clone())?
+    };
+
+    Ok(webview)
+}
 
 fn load_html() -> eyre::Result<String> {
-    let foo = env::var("RUST_SCRIPT_PATH")?;
-    let executable = Path::new(&foo);
-    let folder = executable.parent().ok_or_eyre("static error message")?;
+    let buffer = env::var("RUST_SCRIPT_PATH")?;
+    let executable = Path::new(&buffer);
+    let folder = executable
+        .parent()
+        .ok_or_eyre("Unable to get program folder.")?;
     let html = fs::read_to_string(folder.join("index.html"))?;
     let js = fs::read_to_string(folder.join("index.js"))?;
 
@@ -46,19 +71,11 @@ fn main() -> eyre::Result<()> {
         )
         .get_matches();
 
-    let html = load_html()?;
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new()
         .with_title("Rustui")
         .build(&event_loop)?;
-
-    let builder = WebViewBuilder::new()
-        .with_devtools(args.get_flag("debug"))
-        .with_html(&html);
-    #[cfg(not(target_os = "linux"))]
-    let _webview = builder.build(&window).unwrap();
-    #[cfg(target_os = "linux")]
-    let _webview = builder.build_gtk(window.gtk_window()).unwrap();
+    let _webview = build_webview(&window, args.get_flag("debug"))?;
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
